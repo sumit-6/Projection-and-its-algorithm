607dea0cb128b01b4dc24cdaedc0f5a1e177e3d5
sumit-6 <75926522+sumit-6@users.noreply.github.com>
2021-11-12 21:24:31 +0530

    Update 3D_cuboid_simulation.py

--- 3D_cuboid_simulation.py
+++ 3D_cuboid_simulation.py
@@ -2,64 +2,43 @@ from turtle import *
 import numpy as np
 #Here n represents the number of cartesian point to represent the edge of the cuboid.
 n=8
+Length = float(input("Enter the Length of Cuboid: "))
+Breadth = float(input("Enter the Breadth of Cuboid: "))
+Height = float(input("Enter the Height of Cuboid: "))
 ################################################################################
 #x1,x2,x3,x4,x5,x6,x7 and x8 are the values of 8 x-coordinates of the 8 vertices of cuboid. 
 x1=0
-x1=float(x1)
 x2=0
-x2=float(x2)
 x3=0
-x3=float(x3)
 x4=0
-x4=float(x4) 
-x5=-4.5
-x5=float(x5)
-x6=-4.5
-x6=float(x6)
-x7=-4.5
-x7=float(x7)
-x8=-4.5
-x8=float(x8)
+x5=-Length
+x6=-Length
+x7=-Length
+x8=-Length
 #Defining a list containing x-coordinates.
 obj_x=[x1,x2,x3,x4,x5,x6,x7,x8]
 ################################################################################
 #y1,y2,y3,y4,y5,y6,y7 and y8 are the values of 8 y-coordinates of the 8 vertices of cuboid.
 y1=0
-y1=float(y1)
-y2=4.5
-y2=float(y2)
-y3=4.5
-y3=float(y3)
+y2=Breadth
+y3=Breadth
 y4=0
-y4=float(y4)
 y5=0
-y5=float(y5)
-y6=4.5
-y6=float(y6)
-y7=4.5
-y7=float(y7)
+y6=Breadth
+y7=Breadth
 y8=0
-y8=float(y8)
 #Defining a list containing y-coordinates.
 obj_y=[y1,y2,y3,y4,y5,y6,y7,y8]
 ################################################################################
 #z1,z2,z3,z4,z5,z6,z7 and z8 are the values of 8 z-coordinates of the 8 vertices of cuboid.
 z1=0
-z1=float(z1)
 z2=0
-z2=float(z2)
-z3=6.2
-z3=float(z3)
-z4=6.2
-z4=float(z4)
+z3=Height
+z4=Height
 z5=0
-z5=float(z5)
 z6=0
-z6=float(z6)
-z7=6.2
-z7=float(z7)
-z8=6.2
-z8=float(z8)
+z7=Height
+z8=Height
 #Defining a list containing z-coordinates.
 obj_z=[z1,z2,z3,z4,z5,z6,z7,z8]
 #################################################################################
@@ -74,22 +53,19 @@ obj_dict=dict(obj_dict)
 ################################################################################
 #Asking the user for the coordinates from where she/he must have been observing the cuboid.
 #For the sake of our convenience, let's call this point as OBSERVER point.
-obs_x=input("Enter x-coordinate of your location")
+obs_x=input("Enter x-coordinate of your location: ")
 obs_x=float(obs_x)
-obs_y=input("Enter y-coordinate of your location")
+obs_y=input("Enter y-coordinate of your location: ")
 obs_y=float(obs_y)
-obs_z=input("Enter z-coordinate of your location")
+obs_z=input("Enter z-coordinate of your location: ")
 obs_z=float(obs_z)
 obs=[obs_x,obs_y,obs_z]
 #################################################################################
 #Now entering The coordinates of the point at which observer is looking at.
 #For the sake of our convenience, we will call it as FOCUS point.
-focus_x=-2.25
-focus_x=float(focus_x)
-focus_y=2.25
-focus_y=float(focus_y)
-focus_z=3.1
-focus_z=float(focus_z)
+focus_x=-Length/2
+focus_y= Breadth/2
+focus_z= Height/2
 focus=[focus_x,focus_y,focus_z]
 #################################################################################
 #Defining a function: coordinater .
@@ -101,7 +77,7 @@ focus=[focus_x,focus_y,focus_z]
 #Let,s call this plane as PROJ_PLANE. 
 u=obs[0]
 v=obs[1]
-w=obs[2]
+w=obs[2]          # this is the implementation of atan2() fuction
 s=focus[0]
 t=focus[1]
 p=focus[2]
@@ -109,7 +85,7 @@ if (p-w)>=0:
     theta = np.arctan(np.sqrt((s-u)**2+(t-v)**2)/(p-w))
 else:
     theta = np.pi + np.arctan(np.sqrt((s-u)**2+(t-v)**2)/(p-w))
-if (s-u)>0 and (t-v)>0:
+if (s-u)>0 and (t-v)>=0:
     phi = np.arctan((t-v)/(s-u))
 elif (s-u)>0 and (t-v)<0:
     phi = (2*np.pi) + np.arctan((t-v)/(s-u))
@@ -144,7 +120,7 @@ def coordinater(x1,y1,z1):
     y_coordinate=(y1-v)*L+y1
     z_coordinate=(z1-w)*L+z1
     return (x_coordinate,y_coordinate,z_coordinate)
-#Creating dictionary object out of these REFL_POINTS.
+#Creatind dictionary object out of these REFL_POINTS.
 refl_dict=[]
 k=65
 for i in range(n):
@@ -186,7 +162,7 @@ def plot_face1(x,y):
     k=65
     screensize(200,200)
     speed(0)
-    color('red')
+    color('orange')
     pensize(1)
     pu()
     goto(x[chr(k)],y[chr(k)])
@@ -206,7 +182,7 @@ def plot_face2(x,y):
     k=69
     screensize(200,200)
     speed(0)
-    color('red')
+    color('orange')
     pensize(1)
     pu()
     goto(x[chr(k)],y[chr(k)])
@@ -224,7 +200,7 @@ def plot_rem(x,y):
     hideturtle()
     screensize(200,200)
     speed(0)
-    color('red')
+    color('orange')
     pensize(1)
     for k in range(65,69,1):
         pu()
